36f45d3b81b10c829f95856b89441f1fdf322050
Kacper Grabiec <grabiec.kacper01@gmail.com>
2022-05-15 19:32:07 +0200

    add apmlitude envelope method

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -1,2 +1,9 @@
+import numpy as np
+
 class FeatureExtractor:
-    pass
+    @staticmethod
+    def amplitude_envelope(signal: np.ndarray, frame_size: int, hop_length: int) -> np.array:
+        ae = []
+        for i in range(0, len(signal), hop_length):
+            ae.append(max(signal[i:i + frame_size]))
+        return np.array(ae)
