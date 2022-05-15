bd7a21a9fc95ba3bbe3f4ca2cc25a73b77622afd
Łukasz Myśliwiec <lmysliwiec10@gmail.com>
2022-05-15 19:33:34 +0200

    feat: add rms feature extractor

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -1,2 +1,8 @@
+import librosa
+import numpy as np
+
+
 class FeatureExtractor:
-    pass
+    @staticmethod
+    def root_mean_square(signal: np.array, frame_size: int, hop_size: int) -> np.array:
+        return librosa.feature.rms(y=signal, frame_length=frame_size, hop_length=hop_size)
